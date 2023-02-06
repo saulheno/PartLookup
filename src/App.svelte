5115516c00fc0b4b2bae8959c5ca2f94e20299d9
Saul <saul.heno@gmail.com>
2023-02-06 19:30:37 +0000

    build

--- src/App.svelte
+++ src/App.svelte
@@ -19,7 +19,7 @@
   </div>
 
   <p>
-    Cool!
+    Cool! Yeah!
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
 
